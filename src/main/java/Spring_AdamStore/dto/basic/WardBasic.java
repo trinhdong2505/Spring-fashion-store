91a155dab30b8578333736d2024f95f1bd2734de
Đồng Quốc Trịnh <trinhdong2505dz@gmail.com>
2025-05-19 21:35:35 +0700

    initial commit

---
+++ src/main/java/Spring_AdamStore/dto/basic/WardBasic.java
@@ -0,0 +1,13 @@
+package Spring_AdamStore.dto.basic;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WardBasic {
+    Long code;
+    String name;
+}
